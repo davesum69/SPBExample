a1f93ace2133d4d7d662120f13b5a61298bc5628
Dave <dave_sum69@hotmail.com>
2026-06-27 14:15:03 +0530

    Initial commit

---
+++ src/main/java/org/example/spbexample/SpbExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.spbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spbexample/SpbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spbexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
